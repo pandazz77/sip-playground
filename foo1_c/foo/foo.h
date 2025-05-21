b9212f2cc4f96e197911b64ba32051dea39f533e
Denis Kalinin <kdv@shipbuilders.ru>
2025-05-21 15:35:24 +0300

    example1: bin str func

--- foo1_c/foo/foo.h
+++ foo1_c/foo/foo.h
@@ -1,3 +1,4 @@
 #pragma once
 
 int foo_add(int a, int b);
+int foo_lenx2(char *str);
--- foo1_c/test.py
+++ foo1_c/test.py
@@ -1,6 +1,7 @@
 import foo
 
 res = foo.foo_add(1,2)
-print(res) # 3
+lenx2 = foo.foo_lenx2(b"12345")
+print(res,lenx2) # 3 10
 
 # no stubs here...
